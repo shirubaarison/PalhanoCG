121092c4c6a424a22f98b9ceb76f7a67ad73f01d
Alysson Silva <shirubaarison@gmail.com>
2025-06-07 14:13:01 -0300

    refactor camera

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -15,7 +15,6 @@ enum class ProjectionType { Perspective, Orthographic };
 
 class Camera {
 public:
-  // construtor
   Camera(int width, int height, glm::vec3 position);
 
   // envia a matriz de visualização * projeção para o shader
@@ -27,49 +26,45 @@ public:
   glm::mat4 getProjectionMatrix() const;
   glm::mat4 getViewMatrix();
   glm::vec3 getPosition() const;
-
-  bool useWireframe = false;
-  bool useOrtho = false; // usar perspectiva por padrão
-  ProjectionType projectionType = ProjectionType::Perspective;
-
-  bool wireframePressed = false;
-
-  void setWidth(int width);
-  void setHeight(int height);
 private:
   int width;
   int height;
 
   // vetores fundamentais da camera
   glm::vec3 position;
-  glm::vec3 target = glm::vec3(0.0f, 0.0f, -1.0f);
+  glm::vec3 target;
   glm::vec3 right;
   glm::vec3 up;
-  glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::vec3 worldUp;
 
   // matrizes calculadas
-  glm::mat4 cameraMatrix = glm::mat4(1.0f);
-  glm::mat4 projection = glm::mat4(1.0f);
+  glm::mat4 cameraMatrix;
+  glm::mat4 projection;
 
+  bool useWireframe;
+  bool useOrtho;
+  ProjectionType projectionType;
+
+  bool wireframePressed;
   // evitar salto de click
   bool firstClick = true;
   
   // ângulos de rotação
-  float yaw = -90.0f; // apontar para o -z negativo
-  float pitch = 0.0f;
+  float yaw; 
+  float pitch;
   
   // Velocidade de movimento
-  float speed = 2.0f;
-  const float sprintMultiplier = 4.0f;
+  float speed;
+  float sprintMultiplier;
 
   // sensibilidade e limitação do ângulo vertical
-  const float sensitivity = 0.1f;   
-  const float maxVerticalAngle = 89.0f; // evitar alinhamento de direction com up
+  float sensitivity;   
+  float maxVerticalAngle; // evitar alinhamento de direction com up
 
   // Para a camera ortografica
-  float rotationAngle = 0.0f;
-  float rotationSpeed = 1.0f;
-  float orthoZoomLevel = 10.0f;
+  float rotationAngle;
+  float rotationSpeed;
+  float orthoZoomLevel;
 
   void processKeyboard(GLFWwindow* window, float deltaTime);
   void processMouse(GLFWwindow* window);
