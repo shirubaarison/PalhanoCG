c50c3b9a7f7f7914c72e181a3a7630a603f95e2e
Alysson Silva <shirubaarison@gmail.com>
2025-07-06 17:37:50 -0300

    chore: add some models

--- src/core/Scene.cpp
+++ src/core/Scene.cpp
@@ -29,7 +29,7 @@ const Skybox& Scene::getSkybox() const { return *skybox; }
 void Scene::init() 
 {
   // TERRENO
-  terrain = new Terrain("assets/heightmaps/heightmap.png", "assets/heightmaps/mato.jpg", 1025, 1025);
+  terrain = new Terrain("assets/heightmaps/heightmap.png", "assets/heightmaps/areia.jpg", 1025, 1025);
 
   // SKYBOX
   skybox = new Skybox("assets/skybox");
@@ -40,18 +40,38 @@ void Scene::init()
   GameObject obj1;
 
   obj1.shader = &manager.getShader("default");
-  obj1.model = &manager.getModel("saitama");
+  obj1.model = &manager.getModel("pendurador");
   obj1.transform.position = {0.0f, 0.0f, 0.0f};
-  obj1.transform.rotation = {90.0f, 0.0f, 0.0f};
+  obj1.transform.scale = {2.0f, 2.0f, 2.0f};
 
   addObject(obj1);
 
   GameObject obj2;
 
   obj2.shader = &manager.getShader("default");
-  obj2.model = &manager.getModel("pig");
-  obj2.transform.position = {2.0f, 0.0f, 5.0f};
-  obj2.transform.scale = {10.0f, 10.0f, 10.0f};
+  obj2.model = &manager.getModel("bike");
+  obj2.transform.position = {2.0f, 2.0f, 5.0f};
+  obj2.transform.scale = {2.0f, 2.0f, 2.0f};
 
   addObject(obj2);
+
+  GameObject obj3;
+
+  obj3.shader = &manager.getShader("default");
+  obj3.model = &manager.getModel("casa1");
+  obj3.transform.rotation = {0.0f, 180.0f, 0.0f};
+  obj3.transform.position = {10.0f, 0.0f, 0.0f};
+  obj3.transform.scale = {0.01f, 0.01f, 0.01f};
+
+  addObject(obj3);
+  
+  GameObject obj4;
+
+  obj4.shader = &manager.getShader("default");
+  obj4.model = &manager.getModel("pig");
+  obj4.transform.rotation = {0.0f, 180.0f, 0.0f};
+  obj4.transform.position = {0.0f, 3.0f, 0.0f};
+  obj4.transform.scale = {10.0f, 10.0f, 10.0f};
+
+  addObject(obj4);
 }
